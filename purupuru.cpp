ae549a7676346566fa2a36d8b55b833e93c79857
Rick Shinmi <77313741+rickshinmi@users.noreply.github.com>
2023-12-03 16:43:56 +0900

    updated purupuru
    made an fuction to calculate delaytime based on servospeed and distance

--- purupuru.cpp
+++ purupuru.cpp
@@ -1,20 +1,19 @@
-
-
-
-
 #include <M5Stack.h>
-
 #include "ServoEasing.hpp"
 #include <Arduino.h>
+
 #define SERVO1_PIN  5
 #define SERVO2_PIN 18
 #define SERVO3_PIN 19
-#define SPEED_IN_PIN A0 // 36/VP
-#define MODE_ANALOG_INPUT_PIN A3 // 39
+#define SPEED_IN_PIN A0
+#define MODE_ANALOG_INPUT_PIN A3
+
 ServoEasing Servo1;
 ServoEasing Servo2;
 
+#define START_DEGREE_VALUE  0
 
+void performpurupuru();
 
 void setup() {
     M5.begin();
@@ -23,33 +22,37 @@ void setup() {
 
     Serial.begin(115200);
 
+    Servo1.attachWithTrim(SERVO1_PIN, 0, START_DEGREE_VALUE, DEFAULT_MICROSECONDS_FOR_0_DEGREE, DEFAULT_MICROSECONDS_FOR_180_DEGREE);
 
-    Servo1.attachWithTrim(SERVO1_PIN, 110, START_DEGREE_VALUE, DEFAULT_MICROSECONDS_FOR_0_DEGREE, DEFAULT_MICROSECONDS_FOR_180_DEGREE);
-    Servo2.attach(SERVO2_PIN, 90, DEFAULT_MICROSECONDS_FOR_0_DEGREE, DEFAULT_MICROSECONDS_FOR_180_DEGREE);
+    if (Servo2.attach(SERVO2_PIN, START_DEGREE_VALUE, DEFAULT_MICROSECONDS_FOR_0_DEGREE, DEFAULT_MICROSECONDS_FOR_180_DEGREE) == INVALID_SERVO) {
+        M5.Lcd.println("Error attaching servo");
+        while (true) {
+            delay(1000);
+        }
+    }
 }
 
-
-void loop()
-{
+void loop() {
     performpurupuru();
-    
 }
-
 
 
 void performpurupuru(int servo1num, int servo2num) {
-  Servo1.setEasingType(EASE_LINEAR);
-  Servo2.setEasingType(EASE_LINEAR);
-  setSpeedForAllServos(180);
-  Serial.write("purupuru");
-  for (int i = 0; i < 5; ++i) {
-    Servo1.setEaseTo(servo1num + 10);
-    delay(180);
-    Servo2.setEaseTo(servo2num + 10);
-    delay(180);
-    Servo1.setEaseTo(servo1num - 10);
-    delay(180);
-    Servo1.setEaseTo(servo2num - 10);
-    delay(180);
-  }
+    Servo1.setEasingType(EASE_CUBIC_IN_OUT);
+    Servo2.setEasingType(EASE_CUBIC_IN_OUT);
+    int servoSpeed = 180;
+    int movingGap = 10
+
+    setSpeedForAllServos(servoSpeed);
+    Serial.write("purupuru");
+    for (int i = 0; i < 5; ++i) {
+        Serial.write("pu");
+        Servo1.startEaseTo(servo1num + movingGap);
+        Servo2.startEaseTo(servo2num + movingGap);
+        delay(movingGap * 1000/servospeed);
+        Serial.write("ru");
+        Servo1.startEaseTo(servo1num);
+        Servo2.startEaseTo(servo2num);
+        delay(movingGap * 1000/servospeed);
+    }
 }
