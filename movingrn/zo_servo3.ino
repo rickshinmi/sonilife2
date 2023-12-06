47bfaec0c16d3d72fc3785aaf87970fe341acc00
RikiSaitou <152765424+RikiSaitou@users.noreply.github.com>
2023-12-07 02:10:42 +0900

    サーボ３の最新

--- movingrn/zo_servo3.ino
+++ movingrn/zo_servo3.ino
@@ -2,20 +2,12 @@
 #include "ServoEasing.hpp"
 #include <Arduino.h>
 #include <ArduinoOSCWiFi.h>
-#include "Ultrasonic.h"
-#include <vector>
-
-// Declare data_list as a global variable
-std::vector<int> data_list;
-
 
 #define SERVO1_PIN  5
 #define SERVO2_PIN 18
 #define SERVO3_PIN 19
-#define DC_MOTERPIN 12
 #define SPEED_IN_PIN A0
 #define MODE_ANALOG_INPUT_PIN A3
-Ultrasonic ultrasonic(22);
 
 ServoEasing Servo1;
 ServoEasing Servo2;
@@ -26,24 +18,23 @@ int servo2CurrentPos = 0;
 int oscServo1Pos = 0;
 int oscServo2Pos = 0;
 
-unsigned long previousMillis = 0;
 // Flag to indicate whether OSC data has been received
 bool oscDataReceived = false;
 
 String ssid = "rikiのiPhone";
 String pwd = "12345678";
-int port = 6666;
+int port = 6668;
 
 
 
 //初回接続時にシリアルモニタにて確認し、書き込む
-IPAddress ip(172, 20, 10, 2);
+IPAddress ip(172, 20, 10, 5);
 IPAddress gateway(172, 20, 10, 1);
 IPAddress subnet(255, 255, 255, 240);
 
 //引数にM4Lに送信したい値を入れて呼び出すと送信される
 void sendOSCData(int d) {
-    OscWiFi.send("172.20.10.3", 5555, "/distance", d);
+    OscWiFi.send("172.20.10.3", 5557, "/distance", d);
 }
 
 //void receiveOSCData() {
@@ -55,8 +46,8 @@ void sendOSCData(int d) {
 void performpurupuru(int servo1num, int servo2num) {
     Servo1.setEasingType(EASE_SINE_IN_OUT);
     Servo2.setEasingType(EASE_SINE_IN_OUT);
-    float servoSpeed = 40.;
-    float movingGap = 35.;
+    float servoSpeed = 37.;
+    float movingGap = 40.;
 
     setSpeedForAllServos(servoSpeed);
     
@@ -73,7 +64,7 @@ void performpurupuru(int servo1num, int servo2num) {
 void performservoActions(int servo1Pre, int servo2Pre, int servo1Post, int servo2Post) {
     Servo1.setEasingType(EASE_QUADRATIC_IN_OUT);
     Servo2.setEasingType(EASE_QUADRATIC_IN_OUT);
-    int servoSpeed = 47;
+    int servoSpeed = 70;
     int movingGap1 = servo1Pre - servo1Post;
     int movingGap2 = servo2Pre - servo2Post;
 
@@ -115,7 +106,7 @@ void oscConnect()
   Serial.println("Subnet address: ");
   Serial.println(WiFi.subnetMask());
 
-  OscWiFi.subscribe(port, "/light1/servo1/angle", [&](int i) //port(6666)の"/light1/servo1/angleでサーボの値を受け取る
+  OscWiFi.subscribe(port, "/light3/servo1/angle", [&](int i) //port(6666)の"/light1/servo1/angleでサーボの値を受け取る
   {
     oscServo1Pos = i; //値をoscServo1Posに代入
     oscDataReceived = true;
@@ -123,7 +114,7 @@ void oscConnect()
   });
 
 
-  OscWiFi.subscribe(port, "/light1/servo2/angle", [&](int i) //port(6666)の"/light1/servo2/angleでサーボの値を受け取る
+  OscWiFi.subscribe(port, "/light3/servo2/angle", [&](int i) //port(6666)の"/light1/servo2/angleでサーボの値を受け取る
   {
     oscServo2Pos = i; //値をoscServo2Posに代入
     oscDataReceived = true; 
@@ -151,45 +142,8 @@ void setup() {
 }
 
 
-void nonBlockingDelay(unsigned long interval) {
-  unsigned long currentMillis = millis();
-  if (currentMillis - previousMillis >= interval) {
-    previousMillis = currentMillis;
-  }
-}
-
-int calculateAverage(const std::vector<int>& data) {
-    int sum = 0;
-    for (int value : data) {
-        sum += value;
-    }
-    return data.empty() ? 0 : sum / data.size();
-}
-
-
-
-void sendAverage(){
-    int RangeInCentimeters = ultrasonic.MeasureInCentimeters();
-  Serial.print("current_value");
-    Serial.println(RangeInCentimeters);
-
-    data_list.push_back(RangeInCentimeters);
-        // リストが30個以上になったら古い値から削除
-        if (data_list.size() > 30) {
-            data_list.erase(data_list.begin());
-        }
-        // 平均値を計算
-        int average_centimeters = calculateAverage(data_list);
-        Serial.print("average_centimeters");
-        Serial.println(average_centimeters);
-        sendOSCData(average_centimeters);
-        nonBlockingDelay(50000);
-}
-
-
 void loop() {
   OscWiFi.update(); // 自動的に送受信するために必須
-  sendAverage();
   Serial.println();
   Serial.print(oscDataReceived);
   Serial.print(" ");
