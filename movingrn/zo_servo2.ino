6f1bc8f0c2dd643d81a36e29eb349223c5a39291
Rick Shinmi <77313741+rickshinmi@users.noreply.github.com>
2023-12-07 19:28:02 +0900

    最小限の機能

--- movingrn/zo_servo2.ino
+++ movingrn/zo_servo2.ino
@@ -9,82 +9,18 @@ const int DEFAULT_MICROSECONDS_FOR_0_DEGREE = 1000;
 const int DEFAULT_MICROSECONDS_FOR_180_DEGREE = 2000;
 
 int oscServo1Pos = 0;
-bool oscDataReceived = false;
-
-String ssid = "rikiのiPhone";
-String pwd = "12345678";
-int port = 6667;
-
-IPAddress ip(172, 20, 10, 4);
-IPAddress gateway(172, 20, 10, 1);
-IPAddress subnet(255, 255, 255, 240);
 
 
 // Define a timer variable
 unsigned long lastOscDataTime = 0;
 unsigned long previousMillis = 0;
-void sendOSCData(int d) {
-    OscWiFi.send("172.20.10.3", 5556, "/distance", d);
-}
-
-void oscConnect() {
-    WiFi.config(ip, gateway, subnet);
-
-    while (WiFi.status() != WL_CONNECTED) {
-        delay(500);
-        Serial.print(".");
-    }
-
-    M5.Lcd.println("");
-    M5.Lcd.println("WiFi connected.");
-    M5.Lcd.println("IP address: ");
-    M5.Lcd.println(WiFi.localIP());
-    M5.Lcd.println("Gateway address: ");
-    M5.Lcd.println(WiFi.gatewayIP());
-    M5.Lcd.println("Subnet address: ");
-    M5.Lcd.println(WiFi.subnetMask());
-    M5.Lcd.println(oscServo1Pos);
-
-    Serial.println("");
-    Serial.println("WiFi connected.");
-    Serial.println("IP address: ");
-    Serial.println(WiFi.localIP());
-    Serial.println("Gateway address: ");
-    Serial.println(WiFi.gatewayIP());
-    Serial.println("Subnet address: ");
-    Serial.println(WiFi.subnetMask());
-
-    OscWiFi.subscribe(port, "/light2/servo1/angle", [&](int i) {
-        oscServo1Pos = i;
-        Serial.println(i);
-        oscDataReceived = true;
-        delay(10);
-    });
-}
-
 void setup() {
     M5.begin();
     M5.Power.begin();
     Serial.begin(115200);
-
-    WiFi.begin(ssid, pwd);
-    oscConnect();
-
-
     pinMode(DC_MOTOR_PIN, OUTPUT);
-    pinMode(DIRECTION_PIN, OUTPUT);
 }
 
-void DCmotor(int speed) {
-    if (speed == 0) {
-        for (int i = 100; i >= 0; --i) {
-            analogWrite(DC_MOTOR_PIN, i);
-            delay(50); // 適切な遅延時間を設定してください
-        }
-    } else {
-        analogWrite(DC_MOTOR_PIN, speed);
-    }
-}
 
 void nonBlockingDelay(unsigned long interval) {
   unsigned long currentMillis = millis();
