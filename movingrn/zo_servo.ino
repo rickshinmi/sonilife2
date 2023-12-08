64e6269335c3b703591234f0468ed84cc6acb334
Rick Shinmi <77313741+rickshinmi@users.noreply.github.com>
2023-12-08 15:46:52 +0900

    Update zo_servo.ino 最新

--- movingrn/zo_servo.ino
+++ movingrn/zo_servo.ino
@@ -15,7 +15,7 @@ Ultrasonic ultrasonic(22);
 ServoEasing Servo1;
 ServoEasing Servo2;
 
-#define START_DEGREE_VALUE 40
+#define START_DEGREE_VALUE  0
 int servo1CurrentPos = 0;
 int servo2CurrentPos = 0;
 int oscServo1Pos = 0;
@@ -26,63 +26,23 @@ bool oscDataReceived = false;
 
 
 void performpurupuru(int servo1num, int servo2num) {
-    Servo1.setEasingType(EASE_QUADRATIC_IN);
-    Servo2.setEasingType(EASE_QUADRATIC_IN);
-    int servoSpeed = 20;
+    Servo1.setEasingType(EASE_CUBIC_IN_OUT);
+    Servo2.setEasingType(EASE_CUBIC_IN_OUT);
+    int servoSpeed = 180;
+    int movingGap = 20;
 
     setSpeedForAllServos(servoSpeed);
     Serial.write("purupuru");
-
-    for (int i = 0; i < 5; ++i) {
-        int randomMove = random(1, 4);
-        int movingGap = random(50, 90);
-        int movingGapver = 40;
-
-        switch (randomMove) {
-            case 1:
-                Serial.write("pu");
-                Servo1.startEaseTo(servo1num + movingGapver);
-                Servo2.startEaseTo(servo2num + movingGap);
-                delay(movingGapver * 1000 / servoSpeed);
-                break;
-
-            case 2:
-                Servo1.startEaseTo(servo1num - movingGapver);
-                Servo2.startEaseTo(servo2num - movingGap);
-                delay(movingGapver * 1000 / servoSpeed);
-                break;
-
-            case 3:
-                Servo1.startEaseTo(servo1num - movingGapver);
-                Servo2.startEaseTo(servo2num + movingGap);
-                delay(movingGapver * 1000 / servoSpeed);
-                Serial.write("ru");
-        }
-
-                Servo1.startEaseTo(servo1num);
-                Servo2.startEaseTo(servo2num);
-                delay(movingGapver * 1000 / servoSpeed);
-                break;
+    for (int i = 0; i < random(5,20); ++i) {
+        Serial.write("pu");
+        Servo1.startEaseTo(servo1num + movingGap);
+        Servo2.startEaseTo(servo2num + movingGap);
+        delay(movingGap * 1000/servoSpeed);
+        Serial.write("ru");
+        Servo1.startEaseTo(servo1num);
+        Servo2.startEaseTo(servo2num);
+        delay(movingGap * 1000/servoSpeed);
     }
-}
-
-void renda(){
-  int movingGap = 20;
-    int servoSpeed = 180;
-  Servo1.setEasingType(EASE_QUADRATIC_IN_OUT);
-  Servo2.setEasingType(EASE_QUADRATIC_IN_OUT);
-  setSpeedForAllServos(servoSpeed);
-  for (int i = 0; i < 10; ++i) {
-    int servoSpeed = 180;
-    Servo1.startEaseTo(40 + movingGap);
-    Servo2.startEaseTo(40 + movingGap);
-    delay(movingGap * 1000 / servoSpeed);
-}
-
-
-
-
-
 }
 
 void DCmoter(int speed){
@@ -97,7 +57,7 @@ void setup() {
 
     Serial.begin(115200);
 
-    Servo1.attachWithTrim(SERVO1_PIN, 40, START_DEGREE_VALUE, DEFAULT_MICROSECONDS_FOR_0_DEGREE, DEFAULT_MICROSECONDS_FOR_180_DEGREE);
+    Servo1.attachWithTrim(SERVO1_PIN, 0, START_DEGREE_VALUE, DEFAULT_MICROSECONDS_FOR_0_DEGREE, DEFAULT_MICROSECONDS_FOR_180_DEGREE);
 
     if (Servo2.attach(SERVO2_PIN, START_DEGREE_VALUE, DEFAULT_MICROSECONDS_FOR_0_DEGREE, DEFAULT_MICROSECONDS_FOR_180_DEGREE) == INVALID_SERVO) {
         M5.Lcd.println("Error attaching servo");
@@ -108,19 +68,8 @@ void setup() {
 }
 
 
-int purupuruCount = 0;
-
 void loop() {
-  performpurupuru(40, 40);
-  purupuruCount++;
-
-  // Check if it's time to execute renda() randomly
-  int randomTrigger = random(3, 8);  // Randomly choose a trigger between 3 and 7
-  if (purupuruCount == randomTrigger) {
-    renda();
-    purupuruCount = 0;  // Reset the counter
-  }
-
-  
-}
-
+    performpurupuru(40,40);
+    delay(random(5000,10000))
+    }
+    
